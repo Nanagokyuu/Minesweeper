a5e6faf347f3a45aba72c8a3db10a3023ab996ff
Nanagokyuu <102863086+Nanagokyuu@users.noreply.github.com>
2025-12-31 00:32:18 +0800

    Add copyright and license information to MinesweeperApp.swift

--- Minesweeper/App/MinesweeperApp.swift
+++ Minesweeper/App/MinesweeperApp.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Nanagokyuu on 2025/12/22.
 //
+//  Copyright (C) 2025 Nanagokyuu
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 
 
 import SwiftUI
